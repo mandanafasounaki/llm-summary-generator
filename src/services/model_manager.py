2fb5cc860dfddde6dec6e83554d04d5c71d07926
Mandana Fasounaki <mandana.fasounaki@gmail.com>
2025-02-15 11:40:03 +0300

    Update model manager with model details

--- src/services/model_manager.py
+++ src/services/model_manager.py
@@ -2,7 +2,9 @@ import os
 from typing import List, Dict
 import logging
 from ..config.settings import settings
-from langchain.chat_models import ChatOpenAI, ChatAnthropic
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import HumanMessage
 
 
 # logging.basicConfig(level=logging.INFO)
@@ -23,7 +25,28 @@ class ModelManager:
         Initialize models
         """
         if os.getenv("OPENAI_KEY"):
-            self.models['openai'] = ChatOpenAI()
+            self.models['openai'] = ChatOpenAI(
+                model='gpt-4o',
+                temperatire=0,
+                timeout=30,
+                max_retries=3
+            )
 
         if os.getenv('ANTHROPIC_KEY'):
-            self.models['anthropic'] = ChatAnthropic()
+            self.models['anthropic'] = ChatAnthropic(
+                model='claude-3-5-sonnet-20241022',
+                temperature=0,
+                timeout=30,
+                max_retries=3,
+                max_tokens=2048
+                )
+    
+    def get_completion(self, model: str, prompt: str):
+        """
+        Get chat completion from specified model.
+        """
+        if model not in self.models:
+            raise ValueError(f"The model {model} is not supported.")
+        
+        try:
+            response = self.models[model].invoke()
