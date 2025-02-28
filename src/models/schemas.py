13d8897cef178bc8110e46a64b35f2bdd857c809
Mandana Fasounaki <mandana.fasounaki@gmail.com>
2025-02-28 19:28:37 +0300

    Cleanup

--- src/models/schemas.py
+++ src/models/schemas.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import List, Literal, Optional, Union
+from typing import List, Literal, Optional
 
 from pydantic import BaseModel, Field, field_validator
 
